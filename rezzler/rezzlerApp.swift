acc7e626d1e188509e95c9330c9afc5f82f103b6
Arnaud Crowther <arnaudcrowther@gmail.com>
2025-12-03 10:50:57 -0500

    Initial Commit

---
+++ rezzler/rezzlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  rezzlerApp.swift
+//  rezzler
+//
+//  Created by Arnaud Crowther on 12/3/25.
+//
+
+import SwiftUI
+
+@main
+struct rezzlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
